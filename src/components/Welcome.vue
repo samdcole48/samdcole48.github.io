6f6d7510d3112b0e44577a5f47f7d05013ec7739
samdcole48 <samdcole48@gmail.com>
2024-05-09 16:15:43 -0500

    added welcome component

---
+++ src/components/Welcome.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+    <div class="welcomeContainer">
+        <div>Hello, I'm Sam Cole</div>
+        <div>I'm an aspring software developer</div>
+        <hr>
+        <div>Please look around and get to know me</div>
+    </div>
+</template>
+
+<style scoped>
+hr {
+    border-color: black;
+}
+</style>
