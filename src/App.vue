112dd7ec3e6e397f1cb8619ef293c7255d06813b
samdcole48 <samdcole48@gmail.com>
2024-05-12 11:53:19 -0500

    added basic nav bar, need to revisit and add links once later pages exist

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <script setup>
 import Welcome from './components/Welcome.vue'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <main>
+  <main class="mainContainer">
+    <NavBar />
     <Welcome />
   </main>
 </template>
 
 <style scoped>
-
+.mainContainer {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.mainContainer > :first-child {
+  flex-shrink: 0;
+}
+.container > :last-child {
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+
+<template>
+    <div class="navBarContainer">
+        <img class="logo" src='@\assets\logo-noBackground.png'>
+        <div class="pages">
+            <a>About</a>
+            <a>Qualifications</a>
+            <a>Portfolio</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.navBarContainer {
+    background-color: black;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100vw;
+}
+.logo {
+    width: 100px; /*need to rethink about sizing, possibly min and max height */
+    height: auto;
+}
+a {
+}
+</style>
